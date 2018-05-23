f204054e37684c8b1bcb7eb87d4a3f8320ce58b0
Антон <strider-13@mail.ru>
2018-05-24 00:12:05 +1000

    fix: Add resetNextLoadedPage() to MovieReviewAdapter class

--- app/src/main/java/com/viatorfortis/popularmovies/rv/MovieReviewAdapter.java
+++ app/src/main/java/com/viatorfortis/popularmovies/rv/MovieReviewAdapter.java
@@ -1,10 +1,12 @@
 package com.viatorfortis.popularmovies.rv;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.viatorfortis.popularmovies.R;
 import com.viatorfortis.popularmovies.models.MovieReview;
@@ -14,26 +16,48 @@ import java.util.List;
 
 public class MovieReviewAdapter extends RecyclerView.Adapter<MovieReviewAdapter.MovieReviewViewHolder> {
     private final ArrayList<MovieReview> mMovieReviewList;
-    public static int mNextPageNumber = 1;
+    private static int mNextPageNumber = 1;
 
     public static int getNextLoadedPageNumber() {
         return mNextPageNumber;
     }
 
-    public MovieReviewAdapter(ArrayList <MovieReview> movieReviewList) {
+    public static void resetNextLoadedPageNumber() {
+        mNextPageNumber = 1;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(MovieReview review);
+    }
+
+    private final ItemClickListener mItemClickListener;
+
+    public MovieReviewAdapter(ArrayList <MovieReview> movieReviewList, ItemClickListener itemClickListener) {
         mMovieReviewList = movieReviewList;
+        mItemClickListener = itemClickListener;
     }
 
 
-    public class MovieReviewViewHolder extends RecyclerView.ViewHolder {
+    public class MovieReviewViewHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
         private final TextView mAuthor;
         private final TextView mContent;
 
         private MovieReviewViewHolder(View itemView) {
             super(itemView);
 
+            itemView.setOnClickListener(this);
+
             mAuthor = itemView.findViewById(R.id.tv_author);
             mContent = itemView.findViewById(R.id.tv_content);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Toast.makeText(, String.valueOf("asasasasasas"), Toast.LENGTH_LONG).show();
+            mItemClickListener.onItemClick(mMovieReviewList.get(getAdapterPosition() ) );
         }
     }
 
