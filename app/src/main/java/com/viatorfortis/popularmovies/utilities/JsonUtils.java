3682fcbb29d0f0bb25cc73458672ea3ef9559dcf
Антон <strider-13@mail.ru>
2018-05-30 22:35:04 +1000

    feat: add parseMovieVideoListJson method to JsonUtils class

--- app/src/main/java/com/viatorfortis/popularmovies/utilities/JsonUtils.java
+++ app/src/main/java/com/viatorfortis/popularmovies/utilities/JsonUtils.java
@@ -9,6 +9,8 @@ import com.viatorfortis.popularmovies.models.Movie;
 import com.viatorfortis.popularmovies.models.MovieListPage;
 import com.viatorfortis.popularmovies.models.MovieReview;
 import com.viatorfortis.popularmovies.models.MovieReviewListPage;
+import com.viatorfortis.popularmovies.models.MovieVideo;
+import com.viatorfortis.popularmovies.models.MovieVideoList;
 
 import java.util.List;
 
@@ -28,5 +30,13 @@ public class JsonUtils {
         MovieReviewListPage movieReviewListPage = gson.fromJson(json, MovieReviewListPage.class);
 
         return movieReviewListPage.mMovieReviewList;
+    }
+
+    public static List<MovieVideo> parseMovieVideoListJson(String json)
+        throws JsonSyntaxException {
+        Gson gson = new Gson();
+        MovieVideoList movieVideoList = gson.fromJson(json, MovieVideoList.class);
+
+        return  movieVideoList.mMovieVideoList;
     }
 }
